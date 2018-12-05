2b2c1dec083e3453ead11554893a92221ba51c17
Lucy E Wright <40594944+lucyewright@users.noreply.github.com>
2018-12-05 16:17:26 +0000

    Update postdata.js

--- src/queries/postdata.js
+++ src/queries/postdata.js
@@ -1,8 +1,8 @@
 const dbConnection = require("../database/db_connection");
-const postData = name => {
+const postData = (fname, lname) => {
   dbConnection.query(
-    "INSERT INTO people (name) VALUES ($1)",
-    [name],
+    "INSERT INTO people (first_name, last_name) VALUES ($1, $2)",
+    [fname, lname],
     (err, res) => {
       if (err) return cb(err);
       cb(null, res);
